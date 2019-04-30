073b0d1323a86118ac5d921911374f902e2fd401
IvanniaEdith <ivannia.arellano@cetys.edu.mx>
2019-04-29 20:01:25 -0700

    pues ya que

---
+++ api/src/Application.kt
@@ -0,0 +1,60 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    routing {
+
+        get("/ventas") {
+            call.respondText("bolsas = 1, goma = 5 ", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/mundo") {
+            call.respond(mapOf("Buenas" to "tardes"))
+        }
+    }
+
+    routing {
+
+        get("/productos") {
+            call.respondText("bolsas, goma", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/hola") {
+            call.respond(mapOf("Buenas" to "tardes"))
+        }
+    }
+}
+
---
+++ api/test/ApplicationTest.kt
@@ -0,0 +1,38 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import io.ktor.features.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+
+class ApplicationTest {
+
+    @Test
+    fun testVenta() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/ventas").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("bolsas = 1, goma = 5 ", response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testProducto() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/productos").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("bolsas, goma", response.content)
+            }
+        }
+    }
+
+
+}
